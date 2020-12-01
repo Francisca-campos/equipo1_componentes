9d5eed1cb30757bae4dd0f178bee555f61efe423
Francisca <al221811678@gmail.com>
2020-12-01 14:58:20 -0600

    icon

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:equipo1_componentes/src/providers/menu_provider.dart';
+import 'package:equipo1_componentes/src/utils/icono_string_util.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -13,41 +14,32 @@ class HomePage extends StatelessWidget {
   }
 
   Widget _lista() {
-    
     // menuProvider.cargarData()
 
     return FutureBuilder(
       future: menuProvider.cargarData(),
       initialData: [],
-      builder: ( context, AsyncSnapshot<List<dynamic>> snapshot ){
-
+      builder: (context, AsyncSnapshot<List<dynamic>> snapshot) {
         return ListView(
-          children: _listaItems( snapshot.data ),
+          children: _listaItems(snapshot.data),
         );
       },
     );
-
-    
   }
 
-  List<Widget> _listaItems( List<dynamic> data) {
-    
+  List<Widget> _listaItems(List<dynamic> data) {
     final List<Widget> opciones = [];
 
-    data.forEach((opt){
-
+    data.forEach((opt) {
       final widgetTemp = ListTile(
-        title: Text( opt['texto'] ),
-        leading: Icon( Icons.account_circle, color: Colors.blue ),
-        trailing: Icon( Icons.keyboard_arrow_right, color: Colors.blue),
-        onTap: () {
-
-        },
+        title: Text(opt['texto']),
+        leading: getIcon(opt['icon']),
+        trailing: Icon(Icons.keyboard_arrow_right, color: Colors.blue),
+        onTap: () {},
       );
 
-      opciones..add( widgetTemp )
-              ..add( Divider() );
+      opciones..add(widgetTemp)..add(Divider());
     });
-    return opciones; 
+    return opciones;
   }
 }
