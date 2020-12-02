5b0b453f6c3144e5a7fb3694c7287cc25164494f
Francisca <al221811678@gmail.com>
2020-12-02 07:41:08 -0600

    navegacion

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:equipo1_componentes/src/pages/alert_page.dart';
 import 'package:equipo1_componentes/src/providers/menu_provider.dart';
 import 'package:equipo1_componentes/src/utils/icono_string_util.dart';
 import 'package:flutter/material.dart';
@@ -21,13 +22,13 @@ class HomePage extends StatelessWidget {
       initialData: [],
       builder: (context, AsyncSnapshot<List<dynamic>> snapshot) {
         return ListView(
-          children: _listaItems(snapshot.data),
+          children: _listaItems(snapshot.data, context),
         );
       },
     );
   }
 
-  List<Widget> _listaItems(List<dynamic> data) {
+  List<Widget> _listaItems(List<dynamic> data, BuildContext context) {
     final List<Widget> opciones = [];
 
     data.forEach((opt) {
@@ -35,7 +36,11 @@ class HomePage extends StatelessWidget {
         title: Text(opt['texto']),
         leading: getIcon(opt['icon']),
         trailing: Icon(Icons.keyboard_arrow_right, color: Colors.blue),
-        onTap: () {},
+        onTap: () {
+          final route = MaterialPageRoute(builder: (context) => AlertPage());
+
+          Navigator.push(context, route);
+        },
       );
 
       opciones..add(widgetTemp)..add(Divider());
