a326e8dcbab62edf24c77e45e7837b3adb189e97
Francisca <al221811678@gmail.com>
2020-12-04 08:12:46 -0600

    tarjetas

--- lib/src/pages/card_page.dart
+++ lib/src/pages/card_page.dart
@@ -7,6 +7,35 @@ class CardPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Cards'),
       ),
+      body: ListView(
+        padding: EdgeInsets.all(10.0),
+        children: <Widget>[_cardTipo1()],
+      ),
+    );
+  }
+
+  Widget _cardTipo1() {
+    return Card(
+      child: Column(
+        children: <Widget>[
+          ListTile(
+            leading: Icon(Icons.photo_album, color: Colors.blue),
+            title: Text('Soy el titulo de esta tarjeta'),
+            subtitle: Text(
+                'Aqui estamos con la descripcion de la tarjeta que quiero que ustedes vean para tener una idea de lo que quiero mostrarles '),
+          ),
+          Row(mainAxisAlignment: MainAxisAlignment.end, children: <Widget>[
+            FlatButton(
+              child: Text('Cancelar'),
+              onPressed: () {},
+            ),
+            FlatButton(
+              child: Text('Ok'),
+              onPressed: () {},
+            ),
+          ])
+        ],
+      ),
     );
   }
 }
