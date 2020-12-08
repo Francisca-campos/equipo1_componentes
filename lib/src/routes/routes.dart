61ddad8e6722c86954d004bdf1ef76357d5007c5
Francisca <al221811678@gmail.com>
2020-12-07 19:28:13 -0600

    lista imagen

--- lib/src/routes/routes.dart
+++ lib/src/routes/routes.dart
@@ -1,6 +1,7 @@
 import 'package:equipo1_componentes/src/pages/animated_container.dart';
 import 'package:equipo1_componentes/src/pages/card_page.dart';
 import 'package:equipo1_componentes/src/pages/input_page.dart';
+import 'package:equipo1_componentes/src/pages/listview_page.dart';
 import 'package:equipo1_componentes/src/pages/slider_page.dart';
 import 'package:flutter/material.dart';
 import 'package:equipo1_componentes/src/pages/alert_page.dart';
@@ -16,5 +17,6 @@ Map<String, WidgetBuilder> getApplicationRoutes() {
     'animatedContainer': (BuildContext context) => AnimateContainerPage(),
     'inputs': (BuildContext context) => InputPage(),
     'slider': (BuildContext context) => SliderPage(),
+    'list': (BuildContext context) => ListaPage(),
   };
 }
